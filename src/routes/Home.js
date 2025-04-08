9e072084d784e79ef53b5009c253c01a2b005e4c
이수아 <esua@isuaui-MacBookAir.local>
2025-04-08 17:47:24 +0900

    Fix homepage

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,14 +4,22 @@ import Movie from '../components/Movie';
 function App() {
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
+    const [rating, setRating] = useState(7);
     const [currentIndex, setCurrentIndex] = useState(0); // 현재 카드 인덱스 상태
 
     useEffect(() => {
-        fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
+        fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=${rating}&sort_by=year`)
             .then((response) => response.json())
             .then((json) => setMovies(json.data.movies))
+            .then(() => console.log(movies))
+            .then(() => console.log(`https://yts.mx/api/v2/list_movies.json?minimum_rating=${rating}&sort_by=year`))
             .finally(() => setLoading(false));
-    }, []);
+    }, [rating]);
+
+    const onChange = (event) => {
+        setRating(parseFloat(event.target.value));
+    }
+
 
     const nextMovies = () => {
         setCurrentIndex((prevIndex) => (prevIndex + 3) % movies.length); // 다음 카드로 이동 (하나씩 이동)
@@ -25,8 +33,11 @@ function App() {
         <div className="container">
             {loading ? <h1>Loading...</h1> : (
                 <div>
-                    <h1> Top {movies.length} Movies </h1>
+                    <h1> Latest {movies.length} Movies </h1>
+                    <input type="range" onChange={onChange} min="7.0" max="9.0" step="0.5"></input>
+                    <label>  minimum rating : {rating.toFixed(1)}</label>
                     <hr></hr>
+
                     <div className="movie-container">
 
                         <button className="arrow1" onClick={prevMovies}>&lt;</button> {/* 왼쪽 화살표 */}
